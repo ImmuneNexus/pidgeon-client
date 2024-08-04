ff2f2ffe19593d6c00c08380cdd3bb4471c94169
Shariah Mirza <shariahmirza@gmail.com>
2024-08-04 01:40:57 -0400

    Updates

--- entries/pages/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<a href="/client" data-svelte-h="svelte-1kfsy6n">Client</a>`;
-});
-export {
-  Page as default
-};
